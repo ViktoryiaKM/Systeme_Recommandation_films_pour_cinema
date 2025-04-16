1cebcde68f62c39cb06422bf0574200b4e1cc16c
Viktoryia Meurville <vik.meurville@gmail.com>
2025-04-16 13:32:51 +0200

    Add files via upload

--- Script_Machine_Learning_recommendation/global_variable.py
+++ Script_Machine_Learning_recommendation/global_variable.py
@@ -1,2 +1,6 @@
+
+## On crée une variable nommée fonction_ml et on lui attribue une chaîne de caractères vide ("").
 fonction_ml=""
+
+## on crée une variable appelée df_active, et on lui donne une tuple vide ()
 df_active=()
